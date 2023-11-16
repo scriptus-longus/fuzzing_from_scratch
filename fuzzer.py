7c24638da2ea6bc727418cc070b55bcc4c1ba522
scriptus-longus <39151069+scriptus-longus@users.noreply.github.com>
2023-11-16 22:29:06 +0100

    Add files via upload
    log mutator and tracer object state

--- fuzzer.py
+++ fuzzer.py
@@ -1,19 +1,19 @@
 import argparse
-from ptrace import debugger
-import random
 import signal
 import os
 import json
 import sys
+import pickle
 
 from tracer import Tracer
 from mutator import Mutator
-from elftools.elf.elffile import ELFFile
-from elftools.elf.elffile import SymbolTableSection
 
 corpus_path = "corpus/"
 log_path = "logs/"
 test_case_path = "cases/"
+mutator_state_file ="mutator_state.sta"
+seed = 1337
+keep_running = True
 
 def make_printable(x):
   return ["{:02x}".format(ord(chr(c))) for c in x] 
@@ -31,7 +31,7 @@ def generate_readable_log(content):
       ret += " ".join(content[(i*8):((i+1)*8)]) + "\n"
 
   return ret
-  
+
 def print_log(content):
   log = generate_readable_log(content)
   print(log)
@@ -41,14 +41,41 @@ def write_log_readable(content, out_file):
   with open(out_file, "a") as f:
     f.write(log + "\n")
 
-def log_fuzzer(state, out_file):
-  pass
+def signal_handler(signum, frame):
+   print("exiting...")
+   log_fuzzer_state({"mutator": mutator, "tracer": tracer}) 
+   keep_running = False
+   sys.exit(0)
+
+def log_fuzzer_state(state):
+  corpus_log = {k: list(state["mutator"].corpus[k]) for k in state["mutator"].corpus}
+
+  mutator_log = {"corpus": corpus_log, "known_points": state["mutator"].known_points}
+  breakpoint_log = {}
+
+  for bp in tracer.breakpoints:
+    breakpoint_log[bp] = [tracer.breakpoints[bp].addr, tracer.breakpoints[bp].instr]
+
+  tracer_log = {"breakpoints": breakpoint_log, "crashes": tracer.unique_crashes}
+
+  mutator_file = open(os.path.join(log_path, "mutator.sta"), "w")
+  tracer_file = open(os.path.join(log_path, "tracer.sta"), "w")
+  json.dump(mutator_log, mutator_file)
+  json.dump(tracer_log, tracer_file)
+   
+  
+def load_fuzzer_state(folder):
+  pass 
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument("--corpus", help="path to sample files", required=True)
+  parser.add_argument("--case_path", help="folder to save test cases to")  # TODO
   parser.add_argument("--logfile", help="default name for logfile (gets saved to logs folder)", default="mylog.log")
+  parser.add_argument("--logfolder", help="name of folder to save logfiles", default="logs") # TODO
+  parser.add_argument("--pickup", help="folder with fuzzer state to pick up from", default="logs") # TODO
   parser.add_argument("--no-print", help="dont print log info while running", action="store_false", dest="log")
+  parser.add_argument("--seed", help="set seed for random") # TEST
   parser.add_argument("target", help="program to fuzz")
   parser = parser.parse_args()
 
@@ -56,26 +83,35 @@ if __name__ == "__main__":
   target = parser.target
   show_log_info = parser.log
   log_file = parser.logfile
+  seed = parser.seed or seed
   keep_fuzzing = True
 
-  mutator = Mutator(corpus)  
-  tracer = Tracer(target)
-  #corpus = get_corpus(corpus)
+  print(seed)
 
+  mutator = Mutator(corpus, test_case_path=test_case_path, seed=seed)  
+  tracer = Tracer(target)
+  
+  signal.signal(signal.SIGINT, signal_handler)
  
   for filename, content in mutator:
+    if not keep_running:
+      break
     crash = tracer.run(filename)
 
     trace = crash["breakpoints"]
 
     mutator.add(content, trace)
 
+
     if crash["crash"]:
-      print(f"[*] crash detected from {corpus_file} at address {hex(crash['addr'])}")
-      content = open(test_path, "rb").read()
+      print(f"[*] crash detected from at address {hex(crash['addr'])}")
       printable_content = make_printable(bytearray(content))
 
       if show_log_info:
         print_log(printable_content)
+    
       write_log_readable(printable_content, os.path.join(log_path, log_file))
+
+
+
  
